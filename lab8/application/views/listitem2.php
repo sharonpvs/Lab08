a4616b7cd16212a439bc9678f830ccf64eab770e
Okitora <chris.m.c.lee@hotmial.com>
2014-11-06 14:31:56 -0800

    edit button removal

--- lab8/application/views/listitem2.php
+++ lab8/application/views/listitem2.php
@@ -4,6 +4,8 @@
     <td>{description}</td>
     <td><img src="/assets/images/{picture}" width="32" height="32"/></td>
     <td><a href="/admin/edit3/{code}">Edit</a></td>
+    <!--
     <td><a href="/admin/edit4/{code}">Better Edit</a></td>   
-    <td><a href="/admin/edit5/{code}">Pretty Edit</a></td>   
+    <td><a href="/admin/edit5/{code}">Pretty Edit</a></td>
+    -->
 </tr>
