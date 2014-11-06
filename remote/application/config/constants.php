bc2d5762da2449fb7cff51dc938b1a6c18923f9f
Okitora <chris.m.c.lee@hotmial.com>
2014-11-06 15:15:13 -0800

    commit 315

--- remote/application/config/constants.php
+++ remote/application/config/constants.php
@@ -37,5 +37,12 @@ define('FOPEN_WRITE_CREATE_STRICT',				'xb');
 define('FOPEN_READ_WRITE_CREATE_STRICT',		'x+b');
 
 
+/**
+ * add RPC client server constants
+ * not sure if this is needed here..............
+ */
+define('RPC_SERVER',        "services.local/schedules");
+define('RPC_PORT',          80);
+
 /* End of file constants.php */
 /* Location: ./application/config/constants.php */
