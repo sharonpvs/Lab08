0891334e367c3fb47d787318d708b06bc86f962e
sharonpvs <sharonpvs@hotmail.com>
2014-11-08 14:47:40 -0800

    auto loaded caboose library and ferryschedule model

--- lab8/application/config/autoload.php
+++ lab8/application/config/autoload.php
@@ -52,7 +52,7 @@ $autoload['packages'] = array();
 |	$autoload['libraries'] = array('database', 'session', 'xmlrpc');
 */
 
-$autoload['libraries'] = array('database','parser','session');
+$autoload['libraries'] = array('database','parser','session', 'caboose');
 
 
 /*
@@ -109,7 +109,7 @@ $autoload['language'] = array();
 |
 */
 
-$autoload['model'] = array('menu','orders','orderitems');
+$autoload['model'] = array('menu','orders','orderitems', 'ferryschedule');
 
 
 /* End of file autoload.php */
