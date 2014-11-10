790e713b4919098a1a7b96f13d4887ba61da5a64
Okitora <chris.m.c.lee@hotmial.com>
2014-11-09 21:00:14 -0800

    i give up

--- lab8/application/controllers/Menuitem.php
+++ lab8/application/controllers/Menuitem.php
@@ -6,3 +6,40 @@
  * and open the template in the editor.
  */
 
+class Menuitems extends CI_Controller
+{
+    function __construct() {
+        parent::__construct();
+    }
+    
+    function index()
+    {
+        $this->load->library('xmlrpc');
+        $this->load->library('xmlrpcs');
+
+        $config['functions']['getItems'] = array('function' => 'Menuitem.getItems');
+        $config['functions']['updateItems'] = array('function' => 'Menuitem.updateItems');
+        $config['object'] = $this;
+
+        $this->xmlrpcs->initialize($config);
+        $this->xmlrpcs->serve();
+    }
+    
+    function getItems()
+    {
+        $parameters = $request->output_parameters();
+
+        $data = $this->menu->getItems();
+
+        $response = array(
+            $data,
+            'struct'
+        );
+        return $this->xmlrpc->send_response($response);
+    }
+    
+    function updateItems()
+    {
+        
+    }
+}
